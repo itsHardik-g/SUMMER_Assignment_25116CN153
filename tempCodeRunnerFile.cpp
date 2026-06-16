7aa255ba0402e8c8ef9e5f25593cc534b631a545
Hardik Gupta <itshardikgupta2001@gmail.com>
2026-06-16 16:35:31 +0530

    initial commit

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,14 @@
+#include <iostrem>
+using namespace std;
+/*
+program to calculate sum of first n natural numbers*/
+int main(){
+    int numbers;
+    cout <<"enter no.of natural numbers sum you want  to print"<<endl;
+    cin>>n;
+    int sum=0;
+    for(int i=1;i>=n;i++){
+        sum=sum+1;    }
+     cout<<"the sum is"<<sum<<endl;
+     return 0;   
+}
